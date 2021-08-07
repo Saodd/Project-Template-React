d1cd6ca7ec893c657fb7e88c8a649bbad102b447
Lin Lan <lanlin@meideng.net>
2021-08-07 17:49:19 +0800

    引入prettier

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,8 @@
-import * as React from "react";
-import styles from "./index.scss"
+import * as React from 'react';
+import styles from './index.scss';
 
 export function App() {
-    return <div>
-        <p className={styles.myClass}>Hello, Lewin!</p>
-    </div>
+  return <div>
+    <p className={styles.myClass}>Hello, Lewin!</p>
+  </div>;
 }
