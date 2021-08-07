21ee8ccdb48a57acbcc8578120f60d14c81210c4
Lin Lan <lanlin@meideng.net>
2021-08-07 15:19:24 +0800

    基本操作

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Project-Template-React
+
 不用脚手架，自己搭一个React项目框架
+
+## 主要特性
+
+1. 支持 `ts`, `tsx`。
+2. 支持 `scss`，以及在js文件中导入。
+3. 支持 `babel`。
+4. 支持 三方库的CDN加速。
+5. 支持 `webpack`多线程编译。
---
+++ src/app/index.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import styles from "./index.scss"
+
+export function App() {
+    return <div>
+        <p className={styles.myClass}>Hello, Lewin!</p>
+    </div>
+}
